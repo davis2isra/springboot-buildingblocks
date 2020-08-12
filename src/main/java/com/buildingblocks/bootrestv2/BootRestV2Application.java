dd914b5e26b813218b3468045a23eaa28539271a
Israel Vences <davidvences2003@yahoo.com.mx>
2020-08-12 16:29:13 -0500

    First Commit

---
+++ src/main/java/com/buildingblocks/bootrestv2/BootRestV2Application.java
@@ -0,0 +1,13 @@
+package com.buildingblocks.bootrestv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootRestV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootRestV2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/buildingblocks/bootrestv2/BootRestV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buildingblocks.bootrestv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootRestV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
