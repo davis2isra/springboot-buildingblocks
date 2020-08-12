263b51478bd53738a0ee2311a9daa787d7b0b48c
David Vences <dvences90@gmail.com>
2020-08-12 18:13:09 -0500

    Second Commit

--- src/main/java/com/buildingblocks/bootrestv2/BootRestV2Application.java
+++ src/main/java/com/buildingblocks/bootrestv2/BootRestV2Application.java
@@ -2,7 +2,7 @@ package com.buildingblocks.bootrestv2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//
 @SpringBootApplication
 public class BootRestV2Application {
 
