76d090c68bac6271aee8a9462e141be41b0d166e
David Vences <dvences90@gmail.com>
2020-08-13 09:42:31 -0500

    Hello world base

---
+++ src/main/java/com/buildingblocks/bootrestv2/hello/HelloWorldController.java
@@ -0,0 +1,19 @@
+package com.buildingblocks.bootrestv2.hello;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    @RequestMapping(method = RequestMethod.GET, path = "/helloworld")
+    public String helloWorld(){
+        return "Hello World";
+    }
+
+    @GetMapping("/helloworld-bean")
+    public UserDetails helloWorldBean() {
+        return new UserDetails("Karla", "Rulas", "Monterrey");
+    }
+}
