41b38ac9b05efcccc08af850e5594f6a7306965d
David Vences <dvences90@gmail.com>
2020-08-21 16:51:38 -0500

    ContentNegitiation-First Commit

--- src/main/java/com/buildingblocks/bootrestv2/entities/Order.java
+++ src/main/java/com/buildingblocks/bootrestv2/entities/Order.java
@@ -23,4 +23,44 @@ public class Order extends RepresentationModel<Order> {
     @ManyToOne(fetch = FetchType.LAZY)
     @JsonIgnore
     private User user;
+
+    public Order(){}
+
+    public Order(String description, User user) {
+        this.description = description;
+        this.user = user;
+    }
+
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Long orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "orderId=" + orderId +
+                ", description='" + description + '\'' +
+                ", user=" + user +
+                '}';
+    }
 }
--- src/main/java/com/buildingblocks/bootrestv2/exceptions/CustomErrorDetails.java
+++ src/main/java/com/buildingblocks/bootrestv2/exceptions/CustomErrorDetails.java
@@ -6,13 +6,52 @@ import lombok.NoArgsConstructor;
 
 import java.util.Date;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
+//@Data
+//@AllArgsConstructor
+//@NoArgsConstructor
 public class CustomErrorDetails {
     private Date timestamp;
     private String message;
     private String errorDetails;
 
+    public CustomErrorDetails(){}
 
+    public CustomErrorDetails(Date timestamp, String message, String errorDetails) {
+        this.timestamp = timestamp;
+        this.message = message;
+        this.errorDetails = errorDetails;
+    }
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Date timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getErrorDetails() {
+        return errorDetails;
+    }
+
+    public void setErrorDetails(String errorDetails) {
+        this.errorDetails = errorDetails;
+    }
+
+    @Override
+    public String toString() {
+        return "CustomErrorDetails{" +
+                "timestamp=" + timestamp +
+                ", message='" + message + '\'' +
+                ", errorDetails='" + errorDetails + '\'' +
+                '}';
+    }
 }
